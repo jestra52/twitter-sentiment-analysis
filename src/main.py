861a090a972f9d25f9d7e28e6ed3ce57ec114e02
Juan Carlos Estrada <jestrada@celerik.com>
2019-05-21 20:35:52 -0500

    ssss

--- src/main.py
+++ src/main.py
@@ -3,52 +3,23 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
+import numpy as np
 import pickle
-import re
-import sys
 # Local packages
 from format_dataset import FormatDataset
 
-DATASET_DIR = sys.argv[1]
-N_GRAM = 2
-
 format_ds = FormatDataset()
-train_tweets, test_tweets = format_ds.load_train_test_imdb_data(DATASET_DIR)
+loaded_vec = TfidfVectorizer(
+    vocabulary=pickle.load(open('reviews_tfidf_features.pkl', 'rb')))
 
-print('TRAIN REVIEW TWEETS:\n', train_tweets.head(10), '\n')
-print('TEST REVIEW TWEETS:\n', test_tweets.head(10), '\n')
+print(loaded_vec)
 
-# Transform each review into TF-IDF vector
-print('Cleaning and vectorizing reviews with n_gram size=%s...' % N_GRAM, '\n')
-vectorizer = TfidfVectorizer(
-    ngram_range=(1, N_GRAM),
-    preprocessor=format_ds.clear_text,
-    stop_words='english')
+# svm = LinearSVC()
+# svm.fit(loaded_vec.get_feature_names(), loaded_vec.vocabulary)
 
-training_features = vectorizer.fit_transform(train_tweets['text'])    
-test_features = vectorizer.transform(test_tweets['text'])
-print(test_features)
-
-# Training with SVM
-print('Training with SVM...\n')
-svm = LinearSVC()
-svm.fit(training_features, train_tweets['sentiment'])
-
-# Testing with SVM
-print('Testing with SVM...')
-y_pred_svm = svm.predict(test_features)
-acc_svm = accuracy_score(test_tweets['sentiment'], y_pred_svm)
-print('SCORE: %s' % (acc_svm * 100))
-print('CONFUSION MATRIX: \n', confusion_matrix(test_tweets['sentiment'], y_pred_svm), '\n')
-
-# Training with MNB
-print('Training with Multinomial Naive-Bayes...\n')
-mnb = MultinomialNB()
-mnb.fit(training_features, train_tweets['sentiment'])
-
-# Testing with MNB
-print('Testing with Multinomial Naive-Bayes...')
-y_pred_mnb = mnb.predict(test_features)
-acc_mnb = accuracy_score(test_tweets['sentiment'], y_pred_mnb)
-print('SCORE: %s' % (acc_mnb * 100))
-print('CONFUSION MATRIX: \n', confusion_matrix(test_tweets['sentiment'], y_pred_mnb), '\n')
+# # Testing with SVM
+# print('Testing with SVM...')
+# y_pred_svm = svm.predict(docs_vec)
+# acc_svm = accuracy_score(test_tweets['sentiment'], y_pred_svm)
+# print('SCORE: %s' % (acc_svm * 100))
+# print('CONFUSION MATRIX: \n', confusion_matrix(test_tweets['sentiment'], y_pred_svm), '\n')
